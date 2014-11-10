3da69d752dce7d9e9c40522150afb9cfd0ce7aa7
rkoteska <rkoteska@propeople.dk>
2014-11-11 00:35:26 +1000

    SOF-10 Theming setup - Header Menu Block exporting feature - medium devices script

--- sites/all/themes/sof_theme/scripts/custom.js
+++ sites/all/themes/sof_theme/scripts/custom.js
@@ -18,14 +18,14 @@
 		
 		//Show / Hide navigation script
 		$("#block-system-main-menu .menu li").hover(function(){
-			$(this).delay(400).toggleClass("slideul");
-			var slideul = $(this).hasClass("slideul") ? true : false;
-			if(slideul){
-				$(this).find('.second-level-main-container').stop(true, true).delay(400).show();
-			}else{
-				$(this).find('.second-level-main-container').hide();
-			}
-		});
+				$(this).toggleClass("slideul");
+				var slideul = $(this).hasClass("slideul") ? true : false;
+				if(slideul){
+					$(this).find('.second-level-main-container').stop(true).slideDown();
+				}else{
+					$(this).find('.second-level-main-container').slideUp();
+				}
+			});	
 			
 		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ){
 			//Show / Hide navigation script on iphones
@@ -33,7 +33,7 @@
 				$(this).toggleClass("slideul");
 				var slideul = $(this).hasClass("slideul") ? true : false;
 				if(slideul){
-					$(this).find('.second-level-main-container').stop(true, true).delay(400).slideDown();
+					$(this).find('.second-level-main-container').stop(true).slideDown();
 				}else{
 					$(this).find('.second-level-main-container').slideUp();
 				}
