8b2af905bb265a060edc45a7b4baabb70f307dce
Goce <gsutinoski@propeople.dk>
2014-11-11 12:30:06 +0100

    SOF-44 - Social media block fix path and images in tpl

--- sites/all/modules/custom/sof_footer_social_media/theme/sof_footer_social_media.tpl.php
+++ sites/all/modules/custom/sof_footer_social_media/theme/sof_footer_social_media.tpl.php
@@ -15,7 +15,7 @@
 	<?php foreach ($items as $key=>$item) : ?>
 		 <div class="footer_social_media_<?php print $key; ?>" >
 		   <div class="sof_footer_social_media_icon" >
-		       <img src="<?php print $item['icon']; ?>" width="80px" height="80px" />
+		       <img src="<?php print $item['icon']; ?>" height="50px" />
 		   </div>
 		   <h5><?php print $item['title']?></h5>  
 		   <p><?php print $item['description']?></p>
