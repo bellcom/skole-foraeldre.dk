44049dd573784e19a0d79b4d3dac07787225ec47
Goce <gsutinoski@propeople.dk>
2014-12-22 11:57:47 +0100

    SOF-74 - Toolbox search php warning. Known array rendering issue

--- sites/all/modules/custom/sof_toolbox_search/theme/sof_toolbox_search.tpl.php
+++ sites/all/modules/custom/sof_toolbox_search/theme/sof_toolbox_search.tpl.php
@@ -1,3 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for the pane.
+ *
+ * @author Goce Shutinoski <gsutinoski@propeople.dk>
+ */
+
+?>
 <div class="sof_toolbox_wrapper" >
     <?php foreach ($items as $item) : ?>
     <?php print render($item); ?>
