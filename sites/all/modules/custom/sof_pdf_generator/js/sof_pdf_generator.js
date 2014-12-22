c3d412afd59a9b7970cf8598fc3abd9914d0259d
Goce <gsutinoski@propeople.dk>
2014-12-22 15:57:06 +0100

    SOF-14 - Font issue fix + coding standards updated

--- sites/all/modules/custom/sof_pdf_generator/js/sof_pdf_generator.js
+++ sites/all/modules/custom/sof_pdf_generator/js/sof_pdf_generator.js
@@ -1,17 +1,21 @@
+/**
+ * @file
+ * SOF pdf generator behaviours
+ */
 (function($) {
 
     Drupal.behaviors.sof_pdf_generator = {
 
         attach : function(context, settings) {
 
-            //Split the body content in two columns
+            // Split the body content in two columns.
             if (typeof Drupal.settings.sof_pdf_generator !== 'undefined') {
                 $('.sof-column-content', context).columnize({
                     columns : 2
                 });
             }
 
-            //Add clases to download links
+            // Add clases to download links.
             $('a[href*="sof-pdf-generate"]', context).addClass('sof-pdf-generate-inline-link');
 
         }
