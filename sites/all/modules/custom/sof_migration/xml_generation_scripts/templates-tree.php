b034f66aef5025c56a2e4133df0618ca29735661
Lachezar Valchev <lachezar@propeople.dk>
2015-01-21 11:17:18 +0200

    SOF-208 Code review clean ups.

--- sites/all/modules/custom/sof_migration/xml_generation_scripts/templates-tree.php
+++ sites/all/modules/custom/sof_migration/xml_generation_scripts/templates-tree.php
@@ -1,102 +1,83 @@
 <?php
-$export = array (
-  'artikel' => 
-  array (
+
+$export = array(
+  'artikel' => array(
     'level' => 0,
     'cnt' => 532,
   ),
-  'sektionsforside' => 
-  array (
+  'sektionsforside' => array(
     'level' => 0,
     'cnt' => 10,
   ),
-  'listevisning' => 
-  array (
+  'listevisning' => array(
     'level' => 0,
     'cnt' => 14,
   ),
-  'folder' => 
-  array (
+  'folder' => array(
     'level' => 0,
     'cnt' => 2,
   ),
-  'english' => 
-  array (
+  'english' => array(
     'level' => 0,
     'cnt' => 1,
   ),
-  'newsletterinternal' => 
-  array (
+  'newsletterinternal' => array(
     'level' => 0,
     'cnt' => 54,
   ),
-  'newsletterroot' => 
-  array (
+  'newsletterroot' => array(
     'level' => 0,
     'cnt' => 1,
   ),
-  'coursebase' => 
-  array (
+  'coursebase' => array(
     'level' => 0,
     'cnt' => 1,
   ),
-  'listemappe' => 
-  array (
+  'listemappe' => array(
     'level' => 0,
     'cnt' => 13,
   ),
-  'newsbase' => 
-  array (
+  'newsbase' => array(
     'level' => 0,
     'cnt' => 1,
   ),
-  'eventbase' => 
-  array (
+  'eventbase' => array(
     'level' => 0,
     'cnt' => 1,
   ),
-  'blogindlaeg' => 
-  array (
+  'blogindlaeg' => array(
     'level' => 0,
     'cnt' => 72,
   ),
-  'bloglistevisning' => 
-  array (
+  'bloglistevisning' => array(
     'level' => 0,
     'cnt' => 1,
   ),
-  'fejl' => 
-  array (
+  'fejl' => array(
     'level' => 0,
     'cnt' => 1,
   ),
-  'ingen-adgang' => 
-  array (
+  'ingen-adgang' => array(
     'level' => 0,
     'cnt' => 1,
   ),
-  404 => 
-  array (
+  404 => array(
     'level' => 0,
     'cnt' => 1,
   ),
-  'loginsupport' => 
-  array (
+  'loginsupport' => array(
     'level' => 0,
     'cnt' => 3,
   ),
-  'frontpage' => 
-  array (
+  'frontpage' => array(
     'level' => 0,
     'cnt' => 1,
   ),
-  'searchresult' => 
-  array (
+  'searchresult' => array(
     'level' => 0,
     'cnt' => 1,
   ),
-  'loginpage' => 
-  array (
+  'loginpage' => array(
     'level' => 0,
     'cnt' => 1,
   ),
