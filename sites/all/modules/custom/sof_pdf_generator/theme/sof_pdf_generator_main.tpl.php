2d926f59f2656da93d9385faf7d2f2dee4d56782
Goce <gsutinoski@propeople.dk>
2015-02-04 09:07:30 +0100

    SOF-138 - Updated margins, font size and spacing

--- sites/all/modules/custom/sof_pdf_generator/theme/sof_pdf_generator_main.tpl.php
+++ sites/all/modules/custom/sof_pdf_generator/theme/sof_pdf_generator_main.tpl.php
@@ -15,8 +15,7 @@
 
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" version="XHTML+RDFa 1.0" >
   <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
