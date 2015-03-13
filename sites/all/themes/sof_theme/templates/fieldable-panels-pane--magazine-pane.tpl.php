7f388895d510e15194e6ee5ef5739c2c1efcc9b4
rkoteska <rkoteska@propeople.dk>
2015-03-13 18:10:38 +1000

    SOF-306  Publishing date to be displayed on News node view , and adding some changes to magazine deck tpl file

--- sites/all/themes/sof_theme/templates/fieldable-panels-pane--magazine-pane.tpl.php
+++ sites/all/themes/sof_theme/templates/fieldable-panels-pane--magazine-pane.tpl.php
@@ -14,7 +14,13 @@
     <h3 class="fixed-pane-title-magazine"><?php print render($subtitle);?></h2>
     <div class="<?php print $classes; ?>" <?php print $attributes; ?>>
       <?php print render($title_suffix); ?>
+      <?php hide($content['field_magazine_category'])?>
+      <?php hide($content['field_magazine_links'])?>
       <?php print render($content); ?>
+      <div class="mag-deck-right-group">
+          <?php print render($content['field_magazine_category']);?>
+          <?php print render($content['field_magazine_links']);?>
+      </div>
     </div>
   </div>
 </div>
