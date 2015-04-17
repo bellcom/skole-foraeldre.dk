b71ce9b8a1d32308ec8b3616a6919f578b064a25
rkoteska <rkoteska@propeople.dk>
2015-04-17 20:21:13 +1000

    SOF-359 Magazine deck background image not displayed properly on iPad

--- sites/all/themes/sof_theme/templates/fieldable-panels-pane--magazine-pane.tpl.php
+++ sites/all/themes/sof_theme/templates/fieldable-panels-pane--magazine-pane.tpl.php
@@ -6,7 +6,7 @@
  * fieldable-panels-pane--magazine-pane.tpl.php
  */
 ?>
-<div class="magazine-deck-background-wrapper" style="background: url(<?php print $magazine_background; ?>) fixed repeat-x;" >
+<div class="magazine-deck-background-wrapper" style="background-image: url(<?php print $magazine_background; ?>);" >
   <div class="magazine-deck-inner-block">
     <?php if(!empty($panetitle)):?>
       <h2 class="fixed-pane-title"><?php print render($panetitle);?></h2>
