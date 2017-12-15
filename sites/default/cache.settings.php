b000d06b71cb0c9fc317a1057009d8854edadf28
Lachezar Valchev <lachezar.valchev@ffwagency.com>
2017-12-15 16:55:24 +0200

    SOFCS-5 Updated settings php files.

---
+++ sites/default/cache.settings.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Cache backend.
+ */
+
+// Varnish configuration examples.
+$conf['cache_backends'][] = 'sites/all/modules/contrib/varnish/varnish.cache.inc';
+$conf['cache_class_cache_page'] = 'VarnishCache';
+$conf['varnish_control_key'] = '1ace8d07-f515-4c12-bdfe-96ee0160a9c5';
+$conf['reverse_proxy'] = TRUE;
+$conf['reverse_proxy_addresses'] = array('127.0.0.1');
+$conf['varnish_control_terminal'] = '127.0.0.1:6082';
+$conf['varnish_version'] = 3;
+
+
+// Memcached configuration examples.
+$conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
+$conf['cache_default_class'] = 'MemCacheDrupal';
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+$conf['memcache_servers'] = array('127.0.0.1:11211' => 'default');
+$conf['memcache_key_prefix'] = $databases["default"]["default"]["database"];
---
+++ sites/default/dev.settings.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file
+ * The DEV settings.php
+ */
+
+// Database config.
+$databases = array(
+  "default" => array(
+    "default" => array(
+      "database" => "sof",
+      "username" => "drupal",
+      "password" => "drupal",
+      "host" => "mariadb",
+      "port" => "",
+      "driver" => "mysql",
+      "prefix" => "",
+    ),
+  ),
+);
+
+// Common settings for all installations.
+require('common.settings.php');
+require('cache.settings.php');
+
+// Stage File Proxy config - to get images from prod.
+$conf["stage_file_proxy_origin"] = "http://skole-foraeldre.dk";
+
+// Change mail system with the test one to prevent sending not wanted emails.
+$conf["mail_system"] = array(
+  "default-system" => "TestingMailSystem",
+  "webform" => "TestingMailSystem",
+);
+
+// Disable core search index.
+$conf['search_cron_limit'] = '0';
+
+// Cache override.
+$conf['memcache_servers'] = array(
+  'memcached:11211' => 'default',
+);
+
+// Varnish DEV settings.
+$conf['varnish_version'] = 4;
+$conf['varnish_control_terminal'] = 'varnish:6082';
+$conf['varnish_control_key'] = 'secret';
+
+// Overrides solr url variable used to set solr endpoint.
+$conf['apachesolr_environment']['solr']['url'] = 'http://solr:8983/solr/core1';
+
+/**
+ * Cache configuration example.
+ */
+$conf['cache'] = 0;
+$conf['page_compression'] = 0;
+$conf['preprocess_css'] = 0;
+$conf['preprocess_js'] = 0;
