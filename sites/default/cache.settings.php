67449383337714f2958016ebd36290e801bb0dac
Lachezar Valchev <lachezar.valchev@ffwagency.com>
2017-12-18 11:42:47 +0200

    SOFCS-5 Fixed issues with Memcache configs.

--- sites/default/cache.settings.php
+++ sites/default/cache.settings.php
@@ -5,6 +5,20 @@
  * Cache backend.
  */
 
+// Memcached configuration examples.
+include_once('./includes/cache.inc');
+include_once('./sites/all/modules/contrib/memcache/memcache.inc');
+$conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
+$conf['cache_default_class'] = 'MemCacheDrupal';
+$conf['lock_inc'] = 'sites/all/modules/contrib/memcache/memcache-lock.inc';
+$conf['memcache_stampede_protection'] = TRUE;
+$conf['cache_default_class'] = 'MemCacheDrupal';
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+$conf['memcache_servers'] = array(
+  '127.0.0.1:11211' => 'default',
+);
+$conf['memcache_key_prefix'] = $databases["default"]["default"]["database"];
+
 // Varnish configuration examples.
 $conf['cache_backends'][] = 'sites/all/modules/contrib/varnish/varnish.cache.inc';
 $conf['cache_class_cache_page'] = 'VarnishCache';
@@ -13,11 +27,3 @@ $conf['reverse_proxy'] = TRUE;
 $conf['reverse_proxy_addresses'] = array('127.0.0.1');
 $conf['varnish_control_terminal'] = '127.0.0.1:6082';
 $conf['varnish_version'] = 3;
-
-
-// Memcached configuration examples.
-$conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
-$conf['cache_default_class'] = 'MemCacheDrupal';
-$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
-$conf['memcache_servers'] = array('127.0.0.1:11211' => 'default');
-$conf['memcache_key_prefix'] = $databases["default"]["default"]["database"];
