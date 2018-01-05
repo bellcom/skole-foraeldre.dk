40a9dce9af0c430e8b95f47b678f11927a1d0b74
Velizar Zlatev <velizar.zlatev@ffwagency.com>
2018-01-05 19:49:43 +0200

    SOFCS-8 Add hash to settings file

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -276,7 +276,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = '8t5k4MMGQOE9D3G89cMNUaQ-gj_ixqvldzsPNwakcN8';
 
 /**
  * Base URL (optional).
