4a9ba3c427b78a39d49495ff0ce0f13fdddd826a
Morten Nissen <mail@hjemmesidekongen.dk>
2020-05-19 17:26:19 +0200

    Popup - only once per session

--- docroot/sites/all/themes/sof_theme/scripts/popup.js
+++ docroot/sites/all/themes/sof_theme/scripts/popup.js
@@ -1,17 +1,45 @@
 (function() {
+  function getCookie(name) {
+    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+
+    return v ? v[2] : null;
+  }
+  function setCookie(name, value, days) {
+    if (days) {
+      var d = new Date;
+
+      d.setTime(d.getTime() + 24*60*60*1000*days);
+
+      document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
+    }
+    else {
+      document.cookie = name + "=" + value + ";path=/;";
+    }
+  }
+  function deleteCookie(name) {
+    setCookie(name, '', -1);
+  }
+
   document.addEventListener('DOMContentLoaded', function(event) {
     var delay = 1; // In seconds.
     var popup = document.querySelector('.bean-popup');
     var body = document.querySelector('body');
+    var popupIsSeen = getCookie('popupIsSeen');
+
+    if (popupIsSeen === 'yes') {
+      return;
+    }
 
     // The popup does not exist. Abort!
     if (popup === null) {
       return;
     }
 
-    // Open popup after 5 sec.
+    // Open popup after X sec.
     setTimeout(function() {
       body.classList.add('bean-popup--visible');
+
+      setCookie('popupIsSeen', 'yes');
     }, (delay * 1000));
 
     // Don't close on click inside the content area.
